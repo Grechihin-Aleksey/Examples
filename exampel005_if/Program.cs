f8af299089b3f79a291eee1a8259ec27dbcf34ac
Grechihin Alexey <grechihinaleksey@rambler.ru>
2023-06-21 17:00:54 +0300

    add example005_if

---
+++ exampel005_if/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша"){
+    Console.WriteLine("Привет, о это Маша!");
+}
+else
+{
+    Console.WriteLine("здравствуйте!");
+    Console.WriteLine(username);
+}
