93b25d07ac04fd8f4e158a2f1a4a97045db2f2fe
Grechihin Alexey <grechihinaleksey@rambler.ru>
2023-06-22 10:22:55 +0300

    add example_dog

---
+++ example_dog/Program.cs
@@ -0,0 +1,20 @@
+﻿int count = 0, firstFriendSpeed = 1, 
+secondFriendSpeed = 2, dogSpeed = 5, frend = 2;
+double distance = 11000, time = 0;
+while (distance > 10) {
+    if (frend == 2) {
+        time = distance / (secondFriendSpeed + dogSpeed);
+        frend = 1;
+        count++;
+    }
+    if (frend == 1) {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        frend = 2;
+        count++;
+    }
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+}
+
+Console.Write("Собака пробежит: ");
+Console.Write(count);
+Console.Write(" раз. ");
